b90905eab994407f495839676402e0892b5dbb02
Michael Martinez <mike@Michaels-Mac-mini.local>
2020-03-21 10:16:52 +0000

    added writeFile function function, completed symbol less implementation

--- projects/06/HackAssembler/Main/index.js
+++ projects/06/HackAssembler/Main/index.js
@@ -5,16 +5,17 @@ const binaryWriter = require('../Code')
 
 async function main(file) {
   const filePath = path.resolve(file);
+  const targetPath = filePath.replace(/\.asm$/, '.hack');
   const asmFile = await readFile(filePath);
   const fileParser = parser(binaryWriter);
   const hackFile = fileParser(asmFile);
-  console.log(hackFile);
+  await writefile(hackFile, targetPath);
 }
 
 // read file
 async function readFile(filePath) {
   try {
-    const fileContents = await fs.readFileSync(filePath, { encoding: 'utf-8' });
+    const fileContents = await fs.readFileSync(filePath, { encoding: 'utf8' });
     const asmFile = fileContents.split('\n');
     return asmFile;
   } catch (e) {
@@ -23,10 +24,15 @@ async function readFile(filePath) {
   }
 }
 
-// return
-
-
 // write file
-
+async function writefile(hackFile, target) {
+  try {
+    await fs.writeFileSync(target, hackFile.join('\n'), { encoding: 'utf8' });
+  } catch (e) {
+    console.log('error reading file');
+    console.error(e);
+    throw e;
+  }
+}
 
 module.exports = main;
