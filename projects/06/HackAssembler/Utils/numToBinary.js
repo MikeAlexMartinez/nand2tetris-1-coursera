833e3e49f6fd7e821613427f9df216a95a745da7
Michael Martinez <mike@Michaels-Mac-mini.local>
2020-03-21 08:38:08 +0000

    completed number to binary util

--- projects/06/HackAssembler/Utils/numToBinary.js
+++ projects/06/HackAssembler/Utils/numToBinary.js
@@ -1,28 +1,43 @@
-const { floor, log2, pow } = Math;
+const MAX_LENGTH = 15;
+const NEGATIVE_MAX = Math.pow(2, MAX_LENGTH - 1);
 
-function getStart(n) {
-  return floor(log2(n) + 1);
+function positiveNumToBinary(num) {
+  return num.toString(2);
+}
+
+function createPaddingBits(binString) {
+  const missingLength = MAX_LENGTH - binString.length;
+  if (missingLength > 0) {
+    const noughtArray = Array(missingLength);
+    return noughtArray.fill('0').join('');
+  }
+  return binString;
+}
+
+function addPaddingBits(binString) {
+  const paddingBits = createPaddingBits(binString);
+  return paddingBits + binString;
+}
+
+function truncateExtraBits(binString) {
+  return binString.substring(binString.length - MAX_LENGTH);
+}
+
+function setBitLength(binString) {
+  return binString.length <= MAX_LENGTH
+    ? addPaddingBits(binString)
+    : truncateExtraBits(binString);
 }
 
 function numToBinary(address) {
-  let num = Number(address);
-  if (num === 0) return '0';
-  if (num < 0) throw new Error('address value must be greater than 0'); 
-
-  const start = getStart(num);
-  let val = pow(2, start - 1);
-  let binaryString = '';
-  while (val >= 1) {
-    if (num >= val) {
-      binaryString += '1';
-    } else {
-      binaryString += '0';
-    }
-    num -= val;
-    val /= 2;
+  let num = Number(address)
+  if (num < 0) {
+    num += NEGATIVE_MAX;
+    return `1${positiveNumToBinary(num)}`;
+  } else {
+    const binaryBits = positiveNumToBinary(num);
+    return setBitLength(binaryBits);
   }
-
-  return binaryString
 }
 
 module.exports = numToBinary;
--- projects/06/HackAssembler/Utils/test/numToBinary.spec.js
+++ projects/06/HackAssembler/Utils/test/numToBinary.spec.js
@@ -1,10 +1,36 @@
 const numToBinary = require('../numToBinary');
 
 describe('numToBinary()', () => {
-  it('should convert 8 to binary correctly', () => {
-    expect(numToBinary(8)).toEqual('1000');
+  const MAX_NUMBER = Math.pow(2, 15) - 1;
+  
+  describe('base cases', () => {
+    let res;
+
+    beforeEach(() => {
+      res = numToBinary('8');
+    });
+
+    it('should return a string', () => {
+      expect(typeof res).toBe('string');
+    });
+    it('should return a string with 15 characters', () => {
+      expect(res.length).toBe(15);
+    });
+    it('should convert 8 to binary correctly', () => {
+      expect(res).toBe('000000000001000');
+    });
   })
-  it('should convert 5 to binary correctly', () => {
-    expect(numToBinary(5)).toEqual('101');
+
+  describe('edge cases', () => {
+    it('should convert 5 to binary correctly', () => {
+      expect(numToBinary('5')).toEqual('000000000000101');
+    });
+    it('should represent negative numbers in two\'s complement', () => {
+      expect(numToBinary('-10')).toEqual('111111111110110');
+    });
+    it('should truncate numbers who exceed 15 characters', () => {
+      const number = `${MAX_NUMBER+1}`;
+      expect(numToBinary(number)).toEqual('000000000000000');
+    });
   })
 });
